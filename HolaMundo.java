68ca41e5199e0ffd726494690828b1b7986fd086
Luis Mateo <luismateo9594@gmail.com>
2019-06-12 10:08:50 -0400

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,9 +3,9 @@ public class HolaMundo{
 		
       
    	         System.out.println("Nombre: " + args[0] );
-		 System.out.println("Apellido: "  args[1] );
-		 System.out.println("Edad: "  args[2] );
-		 System.out.println("Sexo: " args[3] );
+		 System.out.println("Apellido: "  +args[1] );
+		 System.out.println("Edad: " + args[2] );
+		 System.out.println("Sexo: " +args[3] );
 		
 		//1000260660_LUIS_MATEO_HOOTSUITE
 		//100267532 - Bryan GUZMAN - Facebook
